0c354b4276b58df1cdf4b0e2ce79b4a40b840e53
Rich Tillman <richtillman@gmail.com>
2021-10-26 15:21:18 -0700

    new app

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 
-# PHC POC
+# Phc
 
 This project was generated using [Nx](https://nx.dev).
 
--- libs/uui/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'uui',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/uui'
-}
