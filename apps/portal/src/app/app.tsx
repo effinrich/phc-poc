b8cb71a67fe989873b9a220383b8c6e8392b1bb1
Rich Tillman <richtillman@gmail.com>
2021-11-24 23:06:06 -0800

    updates and a secrets test

--- apps/portal/src/app/app.tsx
+++ apps/portal/src/app/app.tsx
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-import { ReactComponent as Logo } from './logo.svg';
-import star from './star.svg';
+import { ReactComponent as Logo } from './logo.svg'
+import star from './star.svg'
 
 const StyledApp = styled.div`
   font-family: sans-serif;
@@ -130,7 +130,10 @@ const StyledApp = styled.div`
     width: 16px;
     margin-right: 4px;
   }
-`;
+`
+
+const mapsApiKey = process.env.MAPS_API_KEY
+console.log(mapsApiKey)
 
 export function App() {
   return (
@@ -224,7 +227,7 @@ nx affected:e2e
         </details>
       </main>
     </StyledApp>
-  );
+  )
 }
 
-export default App;
+export default App
